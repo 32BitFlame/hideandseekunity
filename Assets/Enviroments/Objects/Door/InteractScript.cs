f6537fc4c5e7d562a416f578c36f6be190f3930f
32BitFlame <47258322+32BitFlame@users.noreply.github.com>
2019-02-24 20:30:24 -0500

    Worked more with doors and added tag to door

--- Assets/Enviroments/Objects/Door/InteractScript.cs
+++ Assets/Enviroments/Objects/Door/InteractScript.cs
@@ -1,29 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class toggle
-{
-    public bool state = new bool();
-    public void Toggle()
-    {
-        if(state == false)
-        {
-            state = true;
-        }
-        else
-        {
-            state = false;
-        }
-    }
 
-}
 public class InteractScript : MonoBehaviour
 {
-    public bool locked;
-    public toggle active;
-    private Vector3 Angle;
-    void FixedUpdate()
+    [SerializeField]
+    private float rotationSpeed = 5f;
+    [SerializeField]
+    private float rotationEnd = 90f;
+    public int rotationDirection;
+    public bool interacted;
+    public Dictionary<GameObject, bool> objectsIn;
+    void Start()
     {
-        
+        transform.tag = "door";
+        try
+        {
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("player"))
+            {
+                objectsIn[obj] = false;
+            }
+        }
+        catch
+        {
+
+        }
+    }
+    void Update()
+    {
+        try
+        {
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("player"))
+            {
+                //May need to change so that new objects are added
+                if ((obj.GetComponent<plr_move>().interactPress == true) && (objectsIn[obj]))
+                {
+                    Interact(obj.transform.position);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+    }
+    void Interact(Vector3 position)
+    {
+        Debug.Log("Interacted");
+        if (interacted == false)
+        {
+
+            interacted = true;
+            Vector3 relative = (position - transform.position).normalized;
+            if (Vector3.Dot(relative, transform.forward) > 0)
+            {
+                print("Front");
+                //Open Front
+                rotationDirection = 1;
+            }
+            else
+            {
+                print("Back");
+                //Open Back
+                rotationDirection = -1;
+            }
+
+        }
+    }
+    void OnTriggerEnter(Collider objectEntered)
+    {
+        if (objectEntered.tag == "player")
+        {
+            objectsIn[objectEntered.gameObject] = true;
+        }
+    }
+    void OnTriggerExit(Collider objectExited)
+    {
+        if (objectExited.tag == "player")
+        {
+            objectsIn[objectExited.gameObject] = false;
+        }
     }
 }
